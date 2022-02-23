87671d28546292da2a163b3082df3584385e915f
AaL <63700874+Miraluthfi@users.noreply.github.com>
2022-02-23 21:18:36 +0700

    update 40-PipDanVirtualenv

--- 40-PipDanVirtualenv.py
+++ 40-PipDanVirtualenv.py
@@ -4,3 +4,6 @@ py -m pip install -- user virtualenv >> agar tidak terjadi error setelah install
 python -m virtualenv venv >> membuat folder venv
 """
 
+import cowsay
+
+cowsay.meow("Bismillah, Saya sedang belajar Python,Semangat Al")

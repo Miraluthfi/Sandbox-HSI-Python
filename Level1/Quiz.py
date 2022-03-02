e9198081ef7700f98cf46d54260bcbeb66de3e4c
AaL <63700874+Miraluthfi@users.noreply.github.com>
2022-03-02 13:10:17 +0700

    Add Quiz Level 1

---
+++ Level1/Quiz.py
@@ -0,0 +1,62 @@
+import random
+
+bank_soal = [{"soal": "Menurut bahasa definisi puasa adalah..... ", 
+         "opsi" : { "A.": "sikap menerima dengan lapang dada", "B.": "menahan diri dari sesuatu", "C.": "mengendalikan diri dari sesuatu", "D.": "berserah diri kepada Allah"}, 
+         "kunci" :"B"},
+         
+         {"soal": "Niat termasuk salah satu dari..... ", 
+        "opsi" : { "A.": "syarat sah puasa", "B.": "anjuran puasa", "C.": "rukun puasa", "D.": "sunnah puasa"}, 
+        "kunci" : "A"},
+             
+         {"soal": "Salah satu syarat sah puasa Ramadhan adalah..... ", 
+         "opsi" : { "A." : "melafadzkan niat dimalam Ramadhan", "B.": "sahur dengan kurma", "C.": "suci dari haid dan nifas", "D.": "orang yang sudah baligh"}, 
+         "kunci" :"C"},
+             
+        {"soal": "Puasa Ramadhan hukumnya adalah..... ", 
+         "opsi" : { "A.": "sunnah", "B.": "mubah", "C.": "wajib", "D.": "makruh"}, 
+         "kunci" :"C"},
+             
+        {"soal": "Jika tidak terlihat hilal maka disyariatkan menyempurnakan bulan Sya'ban..... ",
+         "opsi" : { "A.": "28 hari", "B.": "20 hari", "C.": "29 hari", "D.": "30 hari"}, 
+         "kunci" : "D"}
+]
+
+print("----- Evaluasi Harian HSI -----")
+print("")
+print("Jumlah Soal : 2")
+print("Pilih satu jawaban yang paling tepat!")
+
+pilih = input("Saya siap mengerjakan dengan jujur (y/n)?")
+if pilih == "y" or "Y": 
+     
+    no = 0
+    totalskor = 0
+    for bs in random.sample(bank_soal, 2):   
+        print("")
+        no += 1  
+        print("{}. {}".format(no, bs["soal"]))
+        
+        for key, value in bs["opsi"].items():
+           print(key, value)
+
+                
+        skor = 0
+        pilihan = input("Masukkan Jawaban Anda (A/B/C/D): ")
+
+        if pilihan == bs["kunci"]:
+            print("Thoyyib, Jawaban Anda Benar!")
+            skor +=2
+            print("Skor : ",skor)
+        
+        else:
+            print("Afwan, Jawaban Anda Salah")
+            skor +=1
+            print("Jawaban yang Benar : " + bs["kunci"] )
+            print("Skor : ",skor)
+        totalskor = totalskor+skor
+    
+print("")
+print("----- Alhamdulillah, Evaluasi Harian Selesai -----")
+print("Total Skor Anda :", totalskor)
+print("Barakallahu fiikum ^-^")
+print("")
